ddf55a6c232073ad295c38e0ba7d19a357a738dd
LL\deekandw <deekandw@publicisgroupe.net>
2023-01-02 22:58:45 +0530

    test corrections

--- app/src/main/java/com/project/employeeLogs/domain/usecase/GetEmployeesUseCase.kt
+++ app/src/main/java/com/project/employeeLogs/domain/usecase/GetEmployeesUseCase.kt
@@ -4,9 +4,9 @@ import com.project.employeeLogs.domain.model.EmployeeDomainModel
 import com.project.employeeLogs.domain.repository.EmployeeRepository
 import kotlinx.coroutines.flow.Flow
 
-class GetEmployeesUseCase(private val onScreenRepository: EmployeeRepository) {
+class GetEmployeesUseCase(private val employeeRepository: EmployeeRepository) {
     operator fun invoke(): Flow<List<EmployeeDomainModel>> {
-        return onScreenRepository.getEmployees()
+        return employeeRepository.getEmployees()
     }
 
 }
