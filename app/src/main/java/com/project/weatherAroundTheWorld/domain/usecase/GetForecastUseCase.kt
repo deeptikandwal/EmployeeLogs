2b2b409511e045ed036aa291c681d6ae72e57040
LL\deekandw <deekandw@publicisgroupe.net>
2023-01-04 16:45:25 +0530

    weather app

--- app/src/main/java/com/project/weatherAroundTheWorld/domain/usecase/GetForecastUseCase.kt
+++ app/src/main/java/com/project/weatherAroundTheWorld/domain/usecase/GetForecastUseCase.kt
@@ -2,8 +2,9 @@ package com.project.weatherAroundTheWorld.domain.usecase
 import com.project.weatherAroundTheWorld.domain.model.DailyForecastDomainModel
 import com.project.weatherAroundTheWorld.domain.repository.ForecastRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetForecastUseCase(private val forecastRepository: ForecastRepository)  {
+class GetForecastUseCase @Inject constructor(private val forecastRepository: ForecastRepository)  {
      operator fun invoke(keyForCity: String,apiKey:String): Flow<DailyForecastDomainModel> {
        return forecastRepository.getForecasts(keyForCity,apiKey)
     }
