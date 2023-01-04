ca49afeb65c54da8dad84b75e0167bc8c390c902
LL\deekandw <deekandw@publicisgroupe.net>
2023-01-04 19:57:54 +0530

    weather app changes

--- utils/src/main/java/com/project/weatherAroundTheWorld/utils/ApiConstants.kt
+++ utils/src/main/java/com/project/weatherAroundTheWorld/utils/ApiConstants.kt
@@ -3,5 +3,5 @@ package com.project.weatherAroundTheWorld.utils
 object ApiConstants {
 
     const val WEATHER_URL="http://dataservice.accuweather.com/"
-    const val WEATHER_API_KEY="ROCdGbhLmeHlG3jvsZkstQGgMGgkbVuZ"
+    const val WEATHER_API_KEY="4F0vGeosXfsJCgYrjnAnaCQAuF0cBPTf"
 }
