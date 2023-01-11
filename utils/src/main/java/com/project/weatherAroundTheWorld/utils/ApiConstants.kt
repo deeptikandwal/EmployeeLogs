fd41a0ccb4c91a5e751d080252ce42f00b91b76d
LL\deekandw <deekandw@publicisgroupe.net>
2023-01-11 15:21:58 +0530

    internal modifications

--- utils/src/main/java/com/project/weatherAroundTheWorld/utils/ApiConstants.kt
+++ utils/src/main/java/com/project/weatherAroundTheWorld/utils/ApiConstants.kt
@@ -3,5 +3,5 @@ package com.project.weatherAroundTheWorld.utils
 object ApiConstants {
 
     const val WEATHER_URL="https://dataservice.accuweather.com/"
-    const val WEATHER_API_KEY="NIcYUY9EM6a8KcJo4mAHuGnfMim2U4Hz"
+    const val WEATHER_API_KEY="5Ba6D9FDRncLJgCmGzupoDkP02kd6d4f"
 }
